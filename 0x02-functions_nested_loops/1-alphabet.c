c482ff4f3146a658920cf1a3dc03f97caaaa1937
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-10-19 00:25:24 +0200

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet gam

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - prints lowercase alphabet
+ * print_alphabet - prints lowercase alphabet
  * Return: always 0 (success)
  */
 
 void print_alphabet(void)
 {
 	int a;
+
 	for (a = 'a' ; a <= 'z' ; a++)
 	{
 		_putchar('a');
-		_putchar('\n');
 	}
+	_putchar('\n');
+
 }
