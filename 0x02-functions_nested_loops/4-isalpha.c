5ae34c7ff4ef63308a8308eaa39c1b6ead71e923
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-10-19 01:28:52 +0200

    4. isalpha fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 
 /**
  *_isalpha - checks alphabetival letters
- *@c - character to be checked
+ *@c: character to be checked
  *Return: 0 or 1 depending on condition
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <+ 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
