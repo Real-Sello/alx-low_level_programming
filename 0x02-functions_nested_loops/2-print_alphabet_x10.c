43964ab93c378b6f6cadb1f9630c8c5408f05414
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-10-19 00:51:46 +0200

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 {
 	int a;
 	int z;
-	
+
 	for (z = '0' ; z <= '10' ; z++)
 	{
 		for (a = 'a' ; a <= 'z' ; a++);
 		{
 			_putchar(a);
 		}
-	}
 	_putchar('\n');
+	}
 }
