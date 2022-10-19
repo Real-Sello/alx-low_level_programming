e3b8c463b9e803b8533aaa38d683861c9f595a69
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-10-19 17:52:36 +0200

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints even  number < 4,000,000
+* Return: always 0 (0) (successful)
+*/
+
+int main(void)
+{
+	int i = 0, n = 1, num = 0;
+	int sum = 0;
+
+	while (num < 4000000)
+	{
+		num = i + n;
+		i = n;
+		n = num;
+		if (nu % 2 == 0)
+		sum += num;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
