4441db4438effe07644a5cbc8ec98fae794a7d57
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-10-20 17:03:16 +0200

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: always 0 (successful)
+ */
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
