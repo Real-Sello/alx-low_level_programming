713c6ba58b07d03cf0dfc7f8c63389e94a39fcb8
Sello Prince Moneatse <spmoneatse@gmail.com>
2022-11-10 15:27:58 +0200

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,10 @@
  * @n: int type for size of byte
  *
  * Return: pointer to memory allocated
+ *
+ * If the function fails, it should return NULL
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * if NULL is passed, treat it as an empty string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
