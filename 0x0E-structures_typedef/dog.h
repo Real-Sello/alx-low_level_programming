87c9b1ec1e6227e23462796eb320bc76e3760373
user name <user email>
2022-11-14 19:36:15 +0200

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef _dog_h_
+#define _dog_h_
+#include <stdio.h>
+
+/**
+ * struct dog - dog
+ *
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
