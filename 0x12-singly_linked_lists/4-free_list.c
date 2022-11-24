6fb84ddaf2436b775ffac0e390d838385243a81e
Real-Sello <spmoneatse@gmail.com>
2022-11-24 21:38:20 +0200

    added 4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ *
+ * @head: struct
+ *
+ * Return: free
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
