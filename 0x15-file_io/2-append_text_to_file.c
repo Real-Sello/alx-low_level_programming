a281153e44606cc128656102b0fbdc1b73c5e0dc
Real-Sello <spmoneatse@gmail.com>
2022-12-05 18:47:54 +0200

    added 2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ *
+ * @filename: name of the file
+ *
+ * @text_content: text to put into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t siz = 0;
+	ssize_t siz2 = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (siz2 = 0, ptr = text_content; *ptr; ptr++)
+			siz2++;
+		siz = write(file, text_content, siz2);
+	}
+
+	if (close(file) == -1 || siz2 != siz)
+		return (-1);
+	return (1);
+}
