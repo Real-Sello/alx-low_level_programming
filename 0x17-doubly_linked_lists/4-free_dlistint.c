ca7b5d194487a736a8af66d497bc3f8f54c74912
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-19 08:24:28 +0200

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ *
+ * @head: head of node
+ *
+ * Return: list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp_node = NULL;
+
+	while (head != NULL)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
