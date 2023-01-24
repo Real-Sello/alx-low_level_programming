421100ef858a2a1531f967fbff7bcaac426d1e61
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-24 13:11:06 +0200

    bug fixes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp "https://github.com/Real-Sello/alx-low_level_programming/blob/master/0x18-dynamic_libraries/make_me_win.so"
+wget -P /tmp -q "https://github.com/Real-Sello/alx-low_level_programming/blob/master/0x18-dynamic_libraries/make_me_win.so"
 export LD_PRELOAD="/tmp/make_me_win.so"
