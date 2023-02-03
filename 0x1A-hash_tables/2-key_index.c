278d8c2a96651f494ce40905aea15c4cb3054581
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-03 23:48:39 +0200

    2. key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+* key_index - function that gives the index of the key
+* @key: key of the table
+* @size: size of the array of the hash table
+* Return: the index of the key
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int find_the_index;
+
+	find_the_index = hash_djb2(key);
+	return (find_the_index % size);
+}
